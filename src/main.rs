a992b608d62ad91f85f0fa181371b3ba358ff2b3
Cyryl Płotnicki <cyplo@cyplo.net>
2020-03-23 13:35:57 +0000

    add notes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use structopt::StructOpt;
 use std::{fs, io};
+use walkdir::WalkDir;
 
 fn main() -> Result<(), io::Error> {
     let options = CommandlineOptions::from_args();
 
     let path = fs::canonicalize(options.dir)?;
     println!("processing {:?}", path);
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        // get date from exif
+        // get date from file name
+        // if they don't agree - use exif
+        // if there's no exif - use file name
+    }
     Ok(())
 }
 
