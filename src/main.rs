0f04c9d8afd65a839e0484fdfbfa77811c4bd74a
Cyryl Płotnicki <cyplo@cyplo.net>
2020-03-23 13:24:58 +0000

    scan commandline options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::{fs, io};
+
+fn main() -> Result<(), io::Error> {
+    let options = CommandlineOptions::from_args();
+
+    let path = fs::canonicalize(options.dir)?;
+    println!("processing {:?}", path);
+    Ok(())
+}
+
+#[derive(Clone, StructOpt, Debug)]
+#[structopt(name = "rusorto")]
+pub struct CommandlineOptions {
+    #[structopt( help = "Directory to process", index = 1 )]
+    pub dir: String,
 }
